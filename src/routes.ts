aa67eb4bd42570c055ce247583e5d04e7db513bb
Eric Souza <eric.bh18souza@gmail.com>
2021-07-17 18:58:11 -0300

    Middlewares and auth routes developed

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express'
 
-import userController from './app/controllers/userController'
+import authMiddleware from './app/middlewares/AuthMiddleware'
+
+import UserController from './app/controllers/UserController'
+import AuthController from './app/controllers/AuthController'
+import userController from './app/controllers/UserController'
 
 const router = Router()
 
-router.post('/users', userController.store)
+router.post('/users', UserController.store)
+router.post('/auth', AuthController.authenticate)
+router.get('/users', authMiddleware, userController.index)
 
 export default router
