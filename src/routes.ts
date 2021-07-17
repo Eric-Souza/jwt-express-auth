22a9cd680df2c27c7b7699494acccb1deeca2106
Eric Souza <eric.bh18souza@gmail.com>
2021-07-17 14:39:12 -0300

    Password encryption developed using bcryptjs

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,9 @@
 import { Router } from 'express'
 
+import userController from './app/controllers/userController'
+
 const router = Router()
+
+router.post('/users', userController.store)
 
 export default router
