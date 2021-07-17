98f29c6e43dd85c4e2329c0d67eb1691886cf4c9
Eric Souza <eric.bh18souza@gmail.com>
2021-07-17 14:07:53 -0300

    User model and db migrations developed

--- src/index.ts
+++ src/index.ts
@@ -11,5 +11,5 @@ app.use(express.json())
 app.use(routes)
 
 app.listen(3000, () => {
-  console.log('Server started at http://localhost:3000')
+	console.log('Server started at http://localhost:3000')
 })
